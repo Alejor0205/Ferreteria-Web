7086b030f91ee13d968f9404e0ccd3bac82df00a
Alejor0205 <dlejo.r02p@gmail.com>
2026-02-01 12:34:04 -0500

    feat: :sparkles: comit desde la rama

--- src/main/java/com/bkseducate/securityapp/infrastructure/persistence/adapters/out/persistence/repository/RefreshTokenRepositoryAdapter.java
+++ src/main/java/com/bkseducate/securityapp/infrastructure/persistence/adapters/out/persistence/repository/RefreshTokenRepositoryAdapter.java
@@ -1,4 +1,4 @@
-package com.bkseducate.securityapp.infrastructure.persistence.adapters.out.persistence;
+package com.bkseducate.securityapp.infrastructure.persistence.adapters.out.persistence.repository;
 
 import com.bkseducate.securityapp.domain.model.RefreshToken;
 import com.bkseducate.securityapp.domain.ports.RefreshTokenRepository;
